f4ff58884cbf36d5c0ebe136f606142a7caea6bc
zh0glikk <zhogliksasha3434@gmail.com>
2019-11-22 13:47:54 +0200

    //

--- OnlineChat/OnlineChat/registration.php
+++ OnlineChat/OnlineChat/registration.php
@@ -7,17 +7,23 @@ $dbServername = "localhost";
 
 	$conn = new mysqli($dbServername,$dbUsername,$dbPassword,$dbName); 
 
-	// if(isset($_POST['username']) && isset($_POST['password'])){
+
 		$username=$_POST['username'];
 		$password=$_POST['password'];
 
+		// $sql_select="SELECT username FROM `sync` WHERE username=$username";
+		// $result=$conn->query($sql_select);
 
-		$sql = "INSERT INTO sync (username,password) VALUES ('$username','$password')";  
+			$sql = "INSERT INTO sync (username,password) VALUES ('$username','$password')";
+			$conn->query($sql);
+	
+
+		  
 
 		
-		$conn->query($sql);
+		
 
-	// }
+	
 
 	
 
